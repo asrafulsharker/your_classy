9f71dc908621a23b08633616d06bc5b3d64c29de
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2022-02-19 20:28:17 +0600

    add

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import nav from './component/Nav/nav';
 import { Container,Col, Row } from 'react-bootstrap';
 import Single from './component/Free_software/single_tab';
 import Data from './component/Free_software/data';
+import ClassRecord from './component/Class_Record/class_record';
+import DBMSRecord from './component/Class_Record/DBMS/dbms_recording';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router,Switch , Route} from "react-router-dom";
 function App() {
@@ -12,8 +14,8 @@ function App() {
     <div className="App">
         <Switch>
           <Route path="/" exact component={Home}/>
-          <Route path="/single" exact component={Single}/>
-          <Route path="/software/:slug" exact component={Single}/>
+          <Route path="/class_record" exact component={ClassRecord}/>
+          <Route path="/dbms_recording" exact component={DBMSRecord}/>
         </Switch>
 
        
--- src/component/Nav/nav.js
+++ src/component/Nav/nav.js
@@ -13,8 +13,8 @@ function nav() {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link href="/">Sofware</Nav.Link>
-      <Nav.Link href="#pricing">Features</Nav.Link>
+      <Nav.Link href="/">Software</Nav.Link>
+      <Nav.Link href="/class_record">Class Record</Nav.Link>
       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
