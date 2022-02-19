19d6bfefbf8e8a783c043547a170c16d6f78de04
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2022-02-19 23:22:52 +0600

    add

--- src/component/Nav/nav.js
+++ src/component/Nav/nav.js
@@ -14,8 +14,8 @@ function nav() {
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
       <Nav.Link href="/">Software</Nav.Link>
-      <Nav.Link href="/class_record">Class Record</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+      <Nav.Link href="/class_record">Class Meterial</Nav.Link>
+      <NavDropdown title="Upcoming" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
